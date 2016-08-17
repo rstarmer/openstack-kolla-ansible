a86f555c87b34398579a1af9ab313ab1410d9663
Robert Starmer <rstarmer@gmail.com>
2016-08-17 01:28:30 -0700

    added missing license blocks

--- node_delete.sh
+++ node_delete.sh
@@ -2,6 +2,21 @@
 
 # Based on the model described here:
 # http://docs.hpcloud.com/#helion/operations/remove_node.html
+#   Copyright 2016 Kumulus Technologies <info@kumul.us>
+#   Copyright 2016 Robert Starmer <rstarmer@gmail.com>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 source ~/admin.rc
 compute_nodes=`nova service-list | grep nova-compute | awk '/|/ {print $6}' | tr '\r' ','`
 echo "which compute node do you want to delete from: $compute_nodes"
