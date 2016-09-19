6f748551997fe7d2ca7a89c3874b428f8a11bedd
Robert Starmer <rstarmer@gmail.com>
2016-09-19 02:13:11 -0400

    Add security group update

--- setup_net.sh
+++ setup_net.sh
@@ -15,11 +15,11 @@
 #   limitations under the License.source ~/admin.rc
 
 tenant=`openstack project list -f csv --quote none | grep admin | cut -d, -f1`
-
+public_network=10.1.10
 neutron net-create public --tenant-id ${tenant} --router:external --provider:network_type flat --provider:physical_network physnet1 --shared
-#if segmented network{vlan,vxlan,gre} --provider:segmentation_id ${1}
-neutron subnet-create public 10.1.10.0/24 --tenant-id ${tenant} --allocation-pool start=10.1.10.80,end=10.1.10.99 --dns-nameserver 8.8.8.8 --disable-dhcp
-# --host-route destination=10.0.0.0/8,nexthop=10.1.10.254
+#if segmented network{vlan,vxlan,gre}: --provider:segmentation_id ${segment_id}
+neutron subnet-create public ${public_network}.0/24 --tenant-id ${tenant} --allocation-pool start=${public_network}.80,end=${public_network}.99 --dns-nameserver 8.8.8.8 --disable-dhcp
+# if you need a specific route to get "out" of your public network: --host-route destination=10.0.0.0/8,nexthop=10.1.10.254
 
 neutron net-create private --tenant-id ${tenant}
 neutron subnet-create private 192.168.100.0/24 --tenant-id ${tenant} --dns-nameserver 8.8.8.8 --name private
@@ -29,3 +29,8 @@ neutron router-create pub-router --tenant-id ${tenant}
 neutron router-gateway-set pub-router public
 neutron router-interface-add pub-router private
 
+# Adjust the default security group.  This is not good practice
+default_group=`neutron security-group-list | awk '/ default / {print $2}' | tail -n 1`
+neutron security-group-rule-create --direction ingress --port-range-min 22 --port-range-max 22 --protocol tcp --remote-ip-prefix 0.0.0.0/0 ${default_group} 
+neutron security-group-rule-create --direction ingress --port-range-min 80 --port-range-max 80 --protocol tcp --remote-ip-prefix 0.0.0.0/0 ${default_group}
+neutron security-group-rule-create --direction ingress --port-range-min 443 --port-range-max 443 --protocol tcp --remote-ip-prefix 0.0.0.0/0 ${default_group}
